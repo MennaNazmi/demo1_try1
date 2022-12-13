8ef24adc07b150811de1d18c600147ac882694da
Menna Nazmi <67035783+MennaNazmi@users.noreply.github.com>
2022-12-14 00:16:04 +0200

    Update app.js

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 app.get('/', (req, res) => {
-    res.send('Hello World from MENNA NAZMI')
+    res.send('Hello World from MENNA NAZMI1')
   })
   
   app.listen(port, () => {
